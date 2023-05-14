49efe1b8e6d7a47f6086d848b8c7cec1403ecfc7
metal <metal@DESKTOP-ECM02DI>
2023-05-13 22:08:26 -0300

    Add project files.

---
+++ ConversorDeMoeda/Conversor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorDeMoeda
+{
+    internal class ConversorDeMoeda
+    {
+        public double cotdol, compdol;
+        public static double Soma(double cotdol, double compdol)
+        {
+            double soma = cotdol * compdol;
+            return soma + soma * 6.0 / 100.0;
+        }
+    }
+}
---
+++ ConversorDeMoeda/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorDeMoeda
+{
+    class Conversor
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotdol = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double compdol = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = ConversorDeMoeda.Soma(cotdol, compdol);
+
+            Console.WriteLine("Valor a ser pago em reais = " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
